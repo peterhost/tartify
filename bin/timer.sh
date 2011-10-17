f3e72ab0491ee3744a28a92bb0bbce508e0edcad
Peter Host <peterhost+github@virtualregions.com>
2011-10-17 21:29:13 +0200

    timer.sh -> better timer script for builtin shell functions benchmarks

--- bin/timer.sh
+++ bin/timer.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 pathToHere=$(cd "$(dirname "$0")"; pwd)
 
+    __K_=$( tput setf 0 || tput setaf 0 || : 2>/dev/null )    # [       0m          SET   FG color to BLACK
+    __R_=$( tput setf 4 || tput setaf 1 || : 2>/dev/null )    # [       1m          SET   FG color to RED
+    __C_=$( tput setf 3 || tput setaf 6 || : 2>/dev/null )    # [       6m          SET   FG color to CYAN
+    __W_=$( tput setf 7 || tput setaf 7 || : 2>/dev/null )    # [       7m          SET   FG color to WHITE
+
+   __EM_=$( tput bold    || : 2>/dev/null )                   # [ 1m                BEGIN DOUBLE INTENSITY (bold) mode
+  __EMK_=$__EM_$__K_                                          # [ 1;    20m         SET   FG color to BOLD BLACK
+  __EMR_=$__EM_$__R_                                          # [ 1;    21m         SET   FG color to BOLD RED
+  __EMC_=$__EM_$__C_                                          # [ 1;    26m         SET   FG color to BOLD CYAN
+
+   __NN_=$( tput sgr0   || : 2>/dev/null )                    # [ 0m                RESET ALL attributes
+
 __getTimeNS(){
   # printf %016d ==>  -  : pad right
   #                   0  : pad with rezoes
@@ -10,10 +22,17 @@ __getTimeNS(){
 }
 
 __benchmark(){
+  #set -- $REPLY
+  local iter=$1;
+  shift
+  [ $iter -gt 1 ] 2>/dev/null || { echo "not a valid iteration number: $iter"; return 1;}
+  echo "
+  ${__K_}NB iterations :$__R_$iter$__NN_"
+
   for tested in $*
   do
     __beginT=`__getTimeNS`
-    for ((  iki=0; iki<1000; iki++ ))
+    for ((  iki=0; iki<$iter; iki++ ))
             #do $tested
             do $tested  >/dev/null 2>&1
     done
@@ -22,12 +41,13 @@ __benchmark(){
     __endT=`__getTimeNS`
     __diffT=$(( $__endT - $__beginT ))
 
-    echo "------------
-    NAME    :$tested
-    begin   :$__beginT
-    end     :$__endT
-    diff    :$__diffT
-    moy(ms) :$((   $(( $__diffT / 1000 )) / $(( $iki + 1 ))   ))"
+    printf "%s\n" $__K_"------------
+    NAME (ns) :$__W_$tested
+    ${__K_}begin(ns) :$__C_$__beginT$__NN_
+    ${__K_}end  (ns) :$__C_$__endT$__NN_
+    ${__K_}diff (ns) :$__EMC_$__diffT$__NN_
+    ${__K_}moy  (ns) :$__R_$((   $__diffT   / $(( $iki + 1 ))   ))$__NN_
+    ${__K_}moy  (ms) :$__EMR_$((   $(( $__diffT / 1000 )) / $(( $iki + 1 ))   ))"$__NN_
   done
 }
 
